b88be19f6f1e2c1c8031b6c56fd5083e91540db3
Mark Yatsiuk <markiatsuk@gmail.com>
2022-01-19 10:16:17 +0200

    main

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,45 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryEl = document.querySelector(".gallery");
+const cardsMarkup = createImgCardsMarkup(galleryItems);
 
-console.log(galleryItems);
+galleryEl.addEventListener("click", onGalleryClick);
+galleryEl.insertAdjacentHTML("beforeend", cardsMarkup);
+
+
+function onGalleryClick(event){
+    // event.preventDefault();
+    const isImgEl = event.target.classList.contains("gallery__image");
+
+    if(!isImgEl){
+        return;
+    }
+
+    const urlOriginal = event.target.dataset.source;
+    console.log(urlOriginal);
+    const getAttribute = event.target.getAttribute("src");
+    const isBigImg = getAttribute == urlOriginal ? true : false;
+    console.log(isBigImg)
+}
+
+
+
+
+// console.log(galleryItems);
+function createImgCardsMarkup(galleryItems){
+    
+    return galleryItems.map(({preview, original, description}) => {
+        return   `
+        <div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+        </div>
+        `;
+    }).join('');
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,11 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+// import * as basicLightbox from 'basiclightbox'
+
+// const instance = basicLightbox.create(`
+//     <img src="assets/images/image.png" width="800" height="600">
+// `)
+
+// instance.show();
 
 console.log(galleryItems);
